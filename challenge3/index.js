cc35138b9be11779577a3d4dd5d3ef5110024c7f
Gilbert <shikweg@gmail.com>
2023-12-16 14:20:10 +0300

    added a function called netSalary and dclared a constant for the tax rates

---
+++ challenge3/index.js
@@ -0,0 +1,15 @@
+//Declaring a function
+function netSalary(basicsalary,benefits){
+// declaring constants for tax rates
+const taxbrackets = [
+{min:0, max:24000, rate:0.1},
+{min:24001, max:32333, rate:0.25},
+{min:32334, max:500000, rate:0.3},
+{min:500001, max:800000, rate:0.325},
+{min:800001, max:Infinity, rate:0.35},
+]
+
+
+
+
+}
