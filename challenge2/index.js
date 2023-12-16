8c352796fe30cc405fa432e9172c6e90e5dffda6
Gilbert <shikweg@gmail.com>
2023-12-16 10:20:15 +0300

    added conditional statements to the index.js

--- challenge2/index.js
+++ challenge2/index.js
@@ -2,5 +2,17 @@
 function speeddetector(speed){
   const speedLimit = 70
   let points = 0
+  // adding conditionals
+  if (speed <= speedLimit){
+    console.log("Ok")
+  } else{
+    points = Maths.abs((speed - speedLimit)/5) 
+    if (points > 12) {
+        console.log("License suspended")
+   } else{
+    console.log(`Points: ${points}`)
+   }
+  }
 
 }
+ 
